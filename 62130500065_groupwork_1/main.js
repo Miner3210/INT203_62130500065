07bd5f773d9ce10357b9ce78e54505290ca135d9
Miner3210 <patsakorn.mark@mail.kmutt.ac.th>
2021-02-01 00:58:23 +0700

    [UPDATE] news article search

--- 62130500065_groupwork_1/main.js
+++ 62130500065_groupwork_1/main.js
@@ -1,9 +1,40 @@
-const app = {
-    data() {
-        return {
-            msg: 'Hello, Vue3'
-        }
-    }
-}
 
-Vue.createApp(app).mount('#app')
+// const vm = Vue.createApp({
+//     el: '#imgArrow',
+//     data: {
+//       name: 'Vue.js'
+//     },
+//     // define methods under the `methods` object
+//     methods: {
+//         hover: function hover(element) {
+//             element.setAttribute('src', 'images/arrow1.png');
+//           },
+          
+//         unhover:  function unhover(element) {
+//             element.setAttribute('src','images/arrow.png');
+//           }  
+//       }
+//     }
+//   )
+//   // you can invoke methods in JavaScript too
+// // -> 'Hello Vue.js!'
+
+
+// // const app = {
+// //     data() {
+// //         return {
+// //             msg: 'Hello, Vue3'
+// //         }
+// //     },
+// //     methods: {
+// //        test: function hover(element) {
+// //             element.setAttribute('src', 'images/arrow1.png');
+// //           }
+// //           ,
+// //         eiei:  function unhover(element) {
+// //             element.setAttribute('src','images/arrow.png');
+// //           }
+// //     }
+// // }
+
+// // Vue.createApp(app).mount('#imgArrow')
